039493fcd3724a3c27f984f053dd4fc74e31dc62
fatigurqiti <ho$28!ZT!83>
2023-06-11 18:08:47 +0200

    Add black filter & username text

--- app/src/main/java/com/fdev/fedra/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/fdev/fedra/ui/screens/HomeScreen.kt
@@ -2,14 +2,19 @@ package com.fdev.fedra.ui.screens
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.fdev.fedra.R
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -25,12 +30,53 @@ fun HomeScreen() {
             state = pagerState,
             key = { dummyPhotos[it] }
         ) { index ->
-            Image(
-                painter = painterResource(id = dummyPhotos[index]),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
-            )
+            Box(modifier = Modifier.fillMaxSize()) {
+                Image(
+                    painter = painterResource(id = dummyPhotos[index]),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+
+                GradientBackground()
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .zIndex(3f),
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(text = "username", color = Color.White, modifier = Modifier.padding(20.dp))
+                        Text(text = "username", color = Color.White, modifier = Modifier.padding(20.dp))
+                    }
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(.1f)
+                    )
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun GradientBackground() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        val gradientBrush = Brush.verticalGradient(
+            colors = listOf(Color(0x00000000), Color(0x99000000))
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxHeight(.5f)
+                .fillMaxWidth()
+                .background(brush = gradientBrush)
+                .zIndex(2f)
+        )
     }
 }
